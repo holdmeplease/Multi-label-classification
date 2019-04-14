0b5640b2eb1f501016bee5ac1464f994c646d1a0
kdyang <33001824+kdyang@users.noreply.github.com>
2019-04-14 16:56:24 +0800

    Add files via upload
    训练和测试已经写好，每100步进行一次validation，每1000步存储参数，支持finetune

--- model.py
+++ model.py
@@ -10,8 +10,9 @@ import torchvision as tv
 from torch.utils.data import DataLoader
 import torchvision.datasets as dsets
 from tools import myDataSet
+import os
 
-model_path = './model_pth/vgg_16.pth'#dir to save para
+model_path = './model_para'#dir to save para
 BATCH_SIZE = 100
 LR = 0.01
 EPOCH = 5
@@ -24,19 +25,22 @@ transform = transforms.Compose([
                          std  = [ 0.229, 0.224, 0.225 ]),
     ])
 
-trainData = myDataSet(root='../data/train', label=train_label,transform=transform)
-testData = dsets.ImageFolder(root='../data/test', label=test_label,transform=transform)
+trainData = myDataSet(root='../data/train', label='../data/train/train_label',transform=transform)
+testData = myDataSet(root='../data/test', label='../data/test/test_label',transform=transform)
 
 trainLoader = torch.utils.data.DataLoader(dataset=trainData, batch_size=BATCH_SIZE, shuffle=True,num_workers=2)
 testLoader = torch.utils.data.DataLoader(dataset=testData, batch_size=BATCH_SIZE, shuffle=False)
 
-vgg_16=v_models.vgg16(pretrained=False,num_classes=20)
-vgg_16_temp=v_models.vgg16(pretrained=True)
-pretrained_dict = vgg_16_temp.state_dict()
-modified_dict = vgg_16.state_dict()
-pretrained_dict = {k: v for k, v in pretrained_dict.items() if k !='classifier.6.weight' and k!='classifier.6.bias'}
-modified_dict.update(pretrained_dict)
-vgg_16.load_state_dict(modified_dict)
+vgg_16 = v_models.vgg16(pretrained=False, num_classes=20)
+if os.path.exists(os.path.join(model_path, 'vgg_16.pkl')):
+    vgg_16.load_state_dict(torch.load(os.path.join(model_path, 'vgg_16.pkl')))
+else:
+    vgg_16_temp=v_models.vgg16(pretrained=True)
+    pretrained_dict = vgg_16_temp.state_dict()
+    modified_dict = vgg_16.state_dict()
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k !='classifier.6.weight' and k!='classifier.6.bias'}
+    modified_dict.update(pretrained_dict)
+    vgg_16.load_state_dict(modified_dict)
 vgg_16.cuda()
 
 # Loss and Optimizer
@@ -45,21 +49,23 @@ optimizer = torch.optim.Adam(vgg_16.parameters(), lr=LR)
 
 # Train the model
 for epoch in range(EPOCH):
-#  for i, (images, labels) in enumerate(trainLoader):
-  for images, labels in trainLoader:
-    images = Variable(images).cuda()
-    labels = Variable(labels).cuda()
+    for i, (images, labels) in enumerate(trainLoader):
+    #for images, labels in trainLoader:
+        images = Variable(images).cuda()
+        labels = Variable(labels).cuda()
 
-    # Forward + Backward + Optimize
-    optimizer.zero_grad()
-    outputs = vgg_16(images)
-    loss = cost(outputs, labels)
-    loss.backward()
-    optimizer.step()
-
-#    if (i+1) % 100 == 0 :
-#      print ('Epoch [%d/%d], Iter[%d/%d] Loss. %.4f' %
-#          (epoch+1, EPOCH, i+1, len(trainData)//BATCH, loss.data[0]))
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        outputs = vgg_16(images)
+        loss = cost(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        #validating
+        if (i+1) % 100 == 0 :
+            print ('Epoch [%d/%d], Iter[%d/%d] Loss. %.4f' %
+                (epoch+1, EPOCH, i+1, len(trainData)//BATCH_SIZE, loss.data[0]))
+        if (i+1)%1000 ==0:
+            torch.save(nn.state_dict(), os.path.join(model_path, 'vgg_16.pkl'))
 
 # Test the model
 vgg_16.eval()
@@ -67,18 +73,17 @@ correct = 0
 total = 0
 
 for images, labels in testLoader:
-  images = Variable(images).cuda()
-  outputs = vgg_16(images)
-  _, predicted = torch.max(outputs.data, 1)
-  total += labels.size(0)
-  correct += (predicted.cpu() == labels).sum()
+    images = Variable(images).cuda()
+    outputs = vgg_16(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted.cpu() == labels).sum()
 
 print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
 
 # Save the Trained Model
-torch.save(nn.state_dict(), 'vgg_16.pkl')
+torch.save(nn.state_dict(), os.path.join(model_path, 'vgg_16.pkl'))
 
-summary(vgg_16,(3,224,224))
 
 
 
